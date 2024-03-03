6001ae259dfad4dae6ed1574db07c8b47d140a01
ZINLAK <108427946+ZinLak@users.noreply.github.com>
2024-03-03 21:53:40 +0400

    Add files via upload

--- ADS_Lab.py
+++ ADS_Lab.py
@@ -28,31 +28,39 @@ def process_number(number):
     for i in range(0, len(digits)-1):
         if digits[i] == digits[i+1]:
             swapped_digits = swapped_digits + str(digits[i])
+            print(digits, swapped_digits)
         else:
-            swapped_digits = swapped_digits + str(int(digits[i+1])*e+int(digits[i]))
+            swapped_digits = swapped_digits + str(int(digits[i+1])*e + int(digits[i]))
             e = e * 10
             digits[i],digits[i+1] = digits[i+1],digits[i]
             if len(digits) > i+2:
                 if digits[i+1] != digits[i+2]:
                     swapped_digits = swapped_digits[:-1]
+            print(digits, swapped_digits)
     swapped_digits = int(swapped_digits)
     if max_value < swapped_digits:
         max_value = swapped_digits
-    # print(swapped_digits)
-    # print(convert_number_to_words(swapped_digits))
 
 def process_lexeme(lexeme):
     if len(lexeme) > max_buffer_len:
         print('Лексема имеет длину больше 100 символов!','\nЛексема:',work_buffer,'\nПропускаем\n')
         return
     
-    if len(lexeme) < 2:
-        print('Лексема имеет длину менее 2 символов!','\nЛексема:',work_buffer,'\nПропускаем\n')
-        return
-    
     if is_number(lexeme):
         number = int(lexeme)
-        process_number(number)
+        if str(number).startswith('11'):
+            process_number(number)
+
+def tokenize_line(line):
+    buffer = ''
+    for char in line:
+        if char not in razd:
+            buffer += char
+        elif buffer:
+            yield buffer
+            buffer = ''
+    if buffer:
+        yield buffer
 
 max_buffer_len = 100 # максимальная длинна буфера
 buffer_len = 1 # размер буфера чтения
@@ -63,21 +71,12 @@ razd = [' ', ',', '.', ';', ':', '?', '!', '"']
 max_value = float('-inf')
 
 with open('text.txt', encoding='utf-8') as file:
-    buffer = file.read(buffer_len)
     lexeme_found = False
-    if not buffer:
-        print('\nФайл text.txt в директории проекта пустой. \nДобавьте не пустой файл в директорию или переименуйте существующий *.txt файл.')
-    while buffer:
-        if not buffer:
-            break
-        while buffer not in razd:
-            work_buffer += buffer
-            buffer = file.read(buffer_len)
-            if not buffer:
-                break
-        process_lexeme(work_buffer)
+    for line in file:
+        tokens = tokenize_line(line)
+        for token in tokens:
+            process_lexeme(token)
         work_buffer = ''
-        buffer = file.read(buffer_len)
         lexeme_found = True
 
 if lexeme_found:
@@ -85,3 +84,23 @@ if lexeme_found:
         print('\nОтвет:', convert_number_to_words(max_value))
     else:    
         print('\nЛексем в файле не осталось или лексемы не удовлетворяют условию (в данном случае проверьте файл и(или) замените *.txt на другой). \nПрограмма завершает работу.')
+
+
+
+# with open('text.txt', encoding='utf-8') as file:
+#     buffer = file.read(buffer_len)
+#     lexeme_found = False
+#     if not buffer:
+#         print('\nФайл text.txt в директории проекта пустой. \nДобавьте не пустой файл в директорию или переименуйте существующий *.txt файл.')
+#     while buffer:
+#         if not buffer:
+#             break
+#         while buffer not in razd:
+#             work_buffer += buffer
+#             buffer = file.read(buffer_len)
+#             if not buffer:
+#                 break
+#         process_lexeme(work_buffer)
+#         work_buffer = ''
+#         buffer = file.read(buffer_len)
+#         lexeme_found = True
